5705f56ee148d34213831013ff91b54e64fe1917
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-12-22 15:00:57 +0100

    Upload images and zoom

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ $(window).on('hashchange', selectPage);
 
 selectPage();
 hljs.initHighlightingOnLoad();
+
+// Get the modal
+var modal = document.getElementById('modal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var modalImg = document.getElementById("image-modal");
+var captionText = document.getElementById("caption");
+$('.page-image').click(function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
